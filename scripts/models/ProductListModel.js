9674303f146932734a7895aa0c5636bbd70479cb
jllachf <yacota@gmail.com>
2014-04-05 16:20:18 +0200

    more

---
+++ scripts/models/ProductListModel.js
@@ -0,0 +1,13 @@
+var auction;
+(function (auction) {
+    (function (model) {
+        var ProductListModel = (function () {
+            function ProductListModel() {
+            }
+            return ProductListModel;
+        })();
+        model.ProductListModel = ProductListModel;
+    })(auction.model || (auction.model = {}));
+    var model = auction.model;
+})(auction || (auction = {}));
+//# sourceMappingURL=ProductListModel.js.map
---
+++ scripts/models/ProductModel.js
@@ -0,0 +1,13 @@
+var auction;
+(function (auction) {
+    (function (model) {
+        var ProductModel = (function () {
+            function ProductModel() {
+            }
+            return ProductModel;
+        })();
+        model.ProductModel = ProductModel;
+    })(auction.model || (auction.model = {}));
+    var model = auction.model;
+})(auction || (auction = {}));
+//# sourceMappingURL=ProductModel.js.map
